736af9f60317d9c653b2b210a441fca83093d90d
Hikiroy <achillelamarque@gmail.com>
2013-04-17 08:10:57 +0200

    Amélioration du message de bienvenue

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main()
     bool  done=0;
     std::string commande;
     std::vector<std::string> commandes;
-    std::cout << "---Bienvenue dans crypto---" << std::endl << "Tapez help pour la liste des commandes"<<std::endl;
+    std::cout << "    ---Bienvenue dans cryptox---" << std::endl << "Tapez help pour la liste des commandes"<<std::endl;
     while(!done)
     {
         std::getline(std::cin,commande);
